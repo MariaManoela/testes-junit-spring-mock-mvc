da0614ad2d678ae3f64f01a6f8eba20ed9f12d56
Maria Manoela <manoelamvc@hotmail.com>
2021-12-15 16:10:02 -0300

    :tada: First commit

---
+++ src/main/java/br/sicredi/testsspringweb/TestsSpringWebApplication.java
@@ -0,0 +1,13 @@
+package br.sicredi.testsspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsSpringWebApplication.class, args);
+	}
+
+}
